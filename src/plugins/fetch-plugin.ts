0977e11fad1f1ca1bcedc5e6b81e402d5b9e524e
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-11 14:58:33 +0900

    separate onLoad logic

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -11,74 +11,63 @@ export const fetchPlugin = (inputCode: string) => {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
       // 読み込んだファイルの中にimportなど別のファイルの読み込みがある場合また呼ばれる
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
+      build.onLoad({ filter: /(^index\.js$)/ }, async (args: any) => {
         // ファイルを読み込む前に偽ファイルを返している
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            // ここにimport, require, etcが入っているのでonLoadに戻る
-            // contents: `
-            //   import message from 'tiny-test-pkg';
-            //   console.log(message);
-            // `,
-            // contents: `
-            //   const react = require('react');
-            //   const reactDOM = require('react-dom');
-            //   console.log(react, reactDOM);
-            // `,
-            // contents: `
-            //   import React, {useState} from 'react';
-            //   console.log(React, useState);
-            // `,
-            contents: inputCode,
-          };
-        }
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        console.log('onLoad/.css$/', args);
         // Check to see if we have already fetched this file
         // and if it is in the chache
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
-
         // if it is, return it immediately
         if (cachedResult) {
           return cachedResult;
         }
-
         const { data, request } = await axios.get(args.path);
-
-        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
-
-        console.log('data');
-        console.log(data);
         // g option indicates you replace all the corresponding characters
         const escaped = data
           .replace(/\n/g, '')
           .replace(/"/g, '\\"')
           .replace(/'/g, "\\'");
-        const contents =
-          fileType === 'css'
-            ? //  // alt
-              // `
-              //   const style = document.createElement('style');
-              //   //  Store the value inside double back quotes after expanding?
-              //   style.innerText=\`${data}\`;
-              //   document.head.appendChild(style);
-              // `
-              `
-                const style = document.createElement('style');
-                style.innerText = '${escaped}';
-                document.head.appendChild(style);
-              `
-            : data;
+        const contents = `
+              const style = document.createElement('style');
+              style.innerText = '${escaped}';
+              document.head.appendChild(style);
+            `;
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: contents,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+        // Store response in cache
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
 
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad/.*/', args);
+        // Check to see if we have already fetched this file
+        // and if it is in the chache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        // if it is, return it immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+        const { data, request } = await axios.get(args.path);
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
         // Store response in cache
         await fileCache.setItem(args.path, result);
         return result;
