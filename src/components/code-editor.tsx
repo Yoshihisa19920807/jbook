fd59969520333c2d99618f2d7695bdefe0b890db
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-14 16:18:22 +0900

    implement prettier

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,7 @@
+import { useRef } from 'react';
 import MonacoEditor from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -7,7 +10,9 @@ interface CodeEditorProps {
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
   // ": () => string" indicates that the type is a function whose return value is string?
+  const editorRef = useRef<any>();
   const editorDidMount = (getValue: () => string, monacoEditor: any) => {
+    editorRef.current = monacoEditor;
     console.log(getValue());
     monacoEditor.getModel().updateOptions({
       tabSize: 2,
@@ -20,24 +25,41 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
     });
     // return onChange;
   };
+
+  const onFormatClick = () => {
+    // function_a().function_b()の形で使える
+    const unformatted = editorRef.current.getModel().getValue();
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      singleQuote: true,
+    });
+    console.log(editorRef.current);
+    // onChange(formatted);
+    editorRef.current.getModel().setValue(formatted);
+  };
+
   return (
-    <MonacoEditor
-      editorDidMount={editorDidMount}
-      // value="Inital Value"
-      value={initialValue}
-      theme="dark"
-      language="javascript"
-      height="500px"
-      options={{
-        wordWrap: 'on',
-        minimap: {
-          enabled: false,
-        },
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-      }}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        editorDidMount={editorDidMount}
+        // value="Inital Value"
+        value={initialValue}
+        theme="dark"
+        language="javascript"
+        height="500px"
+        options={{
+          wordWrap: 'on',
+          minimap: {
+            enabled: false,
+          },
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+        }}
+      />
+    </div>
   );
 };
 
