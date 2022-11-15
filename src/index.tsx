ace3b17d5a6ce6fef4d323ab30abaa83492a3537
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-15 13:18:49 +0900

    - delete unnecessary comments - delete unused variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,14 @@
 import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import Preview from './components/preview';
 import bundle from './bundler';
 
 const App = () => {
-  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
-
-  // const startService = async () => {
-  //   ref.current = await esbuild.startService({
-  //     worker: true,
-  //     // node_moduleのファイル構造から推測
-  //     wasmURL: 'https://www.unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-  //   });
-  //   // console.log(service);
-  // };
-
-  // useEffect(() => {
-  //   startService();
-  // }, []);
 
   const onClick = async () => {
     const output = await bundle(input);
