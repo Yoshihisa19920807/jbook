3bbbce41d6f1b7adad3bdc879450a8ca2a90fec4
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-15 17:18:48 +0900

    update constraints when browser window size changes

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,13 +1,41 @@
 import './resizable.css';
 import { ResizableBox } from 'react-resizable';
+import { useEffect, useState } from 'react';
+
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
   children?: React.ReactNode;
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  const getWindowDimensions = () => {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  };
+
+  useEffect(() => {
+    const onResize = () => {
+      setWindowDimensions(getWindowDimensions());
+    };
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
+  }, []);
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  // const size = useWindowSize();
+  console.log('windowDimensions');
+  console.log(windowDimensions);
+  console.log(windowDimensions.height * 0.9);
   return direction === 'vertical' ? (
     <ResizableBox
+      minConstraints={[Infinity, windowDimensions.height * 0.1]}
+      // maxConstraints={[Infinity, window.innerHeight * 0.9]}
+      maxConstraints={[Infinity, windowDimensions.height * 0.9]}
       height={300}
       width={Infinity}
       resizeHandles={['s']}
