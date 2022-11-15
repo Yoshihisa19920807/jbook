298e58033816e94715f92b3cebb532d2c533dac3
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-15 13:31:44 +0900

    - Modulize code editor and preview

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,35 @@
+import * as esbuild from 'esbuild-wasm';
+import { useState } from 'react';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import bundle from '../bundler';
+
+const CodeCell = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = async () => {
+    const output = await bundle(input);
+    setCode(output);
+  };
+
+  return (
+    <div>
+      <CodeEditor
+        initialValue="const a = 'Inital Value';"
+        onChange={(value) => {
+          console.log('value_index');
+          console.log(value);
+          setInput(value);
+        }}
+      />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <Preview code={code} />
+    </div>
+  );
+};
+
+export default CodeCell;
+// ReactDOM.render(<App />, document.querySelector('#root'));
--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,13 @@
 import * as esbuild from 'esbuild-wasm';
-import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import CodeEditor from './components/code-editor';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import Preview from './components/preview';
-import bundle from './bundler';
+import ReactDOM from 'react-dom/client';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="const a = 'Inital Value';"
-        onChange={(value) => {
-          console.log('value_index');
-          console.log(value);
-          setInput(value);
-        }}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
