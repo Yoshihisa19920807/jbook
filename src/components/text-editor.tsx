7d6adbabd7e1e0e006f09d019b53d49fa0ec0932
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-16 15:14:43 +0900

    - toggle view mode and edit mode

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -2,11 +2,28 @@ import { useState } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 const TextEditor: React.FC = () => {
-  return (
-    <div>
-      <MDEditor />
-    </div>
-  );
+  const [editing, setEditing] = useState(false);
+
+  const toggleMode = () => {
+    setEditing(!editing);
+  };
+
+  if (editing) {
+    return (
+      <div>
+        <button onClick={toggleMode}>Toggle Mode</button>
+        <MDEditor />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <button onClick={toggleMode}>Toggle Mode</button>
+        {/* <MDEditor /> */}
+        <MDEditor.Markdown source={'# header'} />
+      </div>
+    );
+  }
 };
 
 export default TextEditor;
