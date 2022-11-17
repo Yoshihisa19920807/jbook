80082091879c5af504f8511f610c3ae01bfe6033
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-17 13:58:08 +0900

    update cell reducer

--- src/state/reducers/cellReducer.ts
+++ src/state/reducers/cellReducer.ts
@@ -1,1 +1,39 @@
-export {};
+import { ActionTypes } from '../action-types';
+import { Action } from '../actions';
+import { Cell } from '../cell';
+
+interface CellsState {
+  // key = id, Cell = data of a cell
+  data: { [key: string]: Cell };
+  loading: boolean;
+  error: string | null;
+  order: string[];
+}
+
+const initialState: CellsState = {
+  data: {},
+  loading: false,
+  error: null,
+  order: [],
+};
+// 引数の初期値設定
+const reducer = (
+  state: CellsState = initialState,
+  action: Action
+): CellsState => {
+  switch (action.type) {
+    case ActionTypes.UPDATE_CELL:
+      return state;
+    case ActionTypes.MOVE_CELL:
+      return state;
+    case ActionTypes.INSERT_CELL_BEFORE:
+      return state;
+    case ActionTypes.DELETE_CELL:
+      return state;
+    default:
+      return state;
+  }
+  return state;
+};
+
+export default reducer;
