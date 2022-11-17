6c1e93326aaaaccc526ed1a677e72c4b3f866a3a
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-17 11:39:13 +0900

    redux scaffold

---
+++ src/state/actions/index.ts
@@ -0,0 +1,37 @@
+import { ActionTypes } from '../action-types';
+import { CellTypes } from '../cell';
+
+interface MoveCellAction {
+  type: ActionTypes.MOVE_CELL;
+  payload: {
+    id: string;
+    direction: 'up' | 'down';
+  };
+}
+
+interface DeleteCellAction {
+  type: ActionTypes.DELETE_CELL;
+  payload: {
+    id: string;
+  };
+}
+
+interface InsertCellBefore {
+  type: ActionTypes.INSERT_CELL_BEFORE;
+  payload: {
+    id: string;
+    type: CellTypes;
+  };
+}
+interface UpdateCellAction {
+  type: ActionTypes.UPDATE_CELL;
+  payload: {
+    id: string;
+  };
+}
+
+export type Action =
+  | MoveCellAction
+  | DeleteCellAction
+  | InsertCellBefore
+  | UpdateCellAction;
