d3a8b45b5ee8caa581bf81466e8f36c06a38a0e2
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-18 09:44:19 +0900

    Replace code with immer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import { ActionTypes } from '../action-types';
 import { Action } from '../actions';
 import { Cell } from '../cell';
@@ -18,49 +19,57 @@ const initialState: CellsState = {
   order: [],
 };
 
-const reducer = (
-  // 引数の初期値設定
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
-  switch (action.type) {
-    case ActionTypes.UPDATE_CELL:
-      const { id, content } = action.payload;
-      return {
-        // // 「他を残す」ことを明示するためにスプレッド記法を使っている。
-        // // stateをまんま返すがdataだけは上書きして返す --- (1)
-        // ...state,
-        // data: {
-        //   // (1)の上書きの内容。同じことをやっている。
-        //   // dataをまんま返すがdata内のaction.payload.idがkeyになっている要素は上書きして返す --- (2)
-        //   ...state.data,
-        //   [action.payload.id]: {
-        //     // (2)の上書きの内容。これまた同じことをやっている。
-        //     // data内のaction.payload.idがkeyになっている要素をまんま返すがcontent部分だけは上書きして返す。
-        //     ...state.data[action.payload.id],
-        //     content: action.payload.content,
-        //   },
-        // },
-        // ↓id, contentを変数に置き換えた上での記述
+const reducer = produce(
+  (
+    // 引数の初期値設定
+    state: CellsState = initialState,
+    action: Action
+  ) => {
+    switch (action.type) {
+      case ActionTypes.UPDATE_CELL:
+        const { id, content } = action.payload;
+        // return {
+        //   // // 「他を残す」ことを明示するためにスプレッド記法を使っている。
+        //   // // stateをまんま返すがdataだけは上書きして返す --- (1)
+        //   // ...state,
+        //   // data: {
+        //   //   // (1)の上書きの内容。同じことをやっている。
+        //   //   // dataをまんま返すがdata内のaction.payload.idがkeyになっている要素は上書きして返す --- (2)
+        //   //   ...state.data,
+        //   //   [action.payload.id]: {
+        //   //     // (2)の上書きの内容。これまた同じことをやっている。
+        //   //     // data内のaction.payload.idがkeyになっている要素をまんま返すがcontent部分だけは上書きして返す。
+        //   //     ...state.data[action.payload.id],
+        //   //     content: action.payload.content,
+        //   //   },
+        //   // },
+        //   // ↓id, contentを変数に置き換えた上での記述
 
-        ...state,
-        data: {
-          ...state.data,
-          [id]: {
-            ...state.data[id],
-            content: content,
-          },
-        },
-      };
-    case ActionTypes.MOVE_CELL:
-      return state;
-    case ActionTypes.INSERT_CELL_BEFORE:
-      return state;
-    case ActionTypes.DELETE_CELL:
-      return state;
-    default:
-      return state;
-  }
-};
+        //   ...state,
+        //   data: {
+        //     ...state.data,
+        //     [id]: {
+        //       ...state.data[id],
+        //       content: content,
+        //     },
+        //   },
+        // };
+        // ↓ write in immer
+
+        // no need
+        state.data[id].content = content;
+        break;
+      case ActionTypes.MOVE_CELL:
+        return state;
+      case ActionTypes.INSERT_CELL_BEFORE:
+        return state;
+      case ActionTypes.DELETE_CELL:
+        return state;
+      default:
+        return state;
+    }
+  },
+  initialState
+);
 
 export default reducer;
