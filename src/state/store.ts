12f56909ada4ea67dd2ab3d8ea495d3be8349d39
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-18 14:48:16 +0900

    tmp test

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,61 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+// Test
+// import { ActionTypes } from './action-types';
 
 // createStore(<reducer>, <initial value>, <middleware or enhancer>)
 // middleware adds extra functionalities to dispatch function
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+// Test
+// store.dispatch({
+//   type: ActionTypes.INSERT_CELL_BEFORE,
+//   payload: {
+//     id: Math.random().toString(),
+//     type: 'code',
+//   },
+// });
+
+// store.dispatch({
+//   type: ActionTypes.INSERT_CELL_BEFORE,
+//   payload: {
+//     id: null,
+//     type: 'text',
+//   },
+// });
+
+// const order_id = store.getState().cells.order[1];
+
+// store.dispatch({
+//   type: ActionTypes.INSERT_CELL_BEFORE,
+//   payload: {
+//     id: order_id,
+//     type: 'code',
+//   },
+// });
+
+// store.dispatch({
+//   type: ActionTypes.MOVE_CELL,
+//   payload: {
+//     id: order_id,
+//     direction: 'up',
+//   },
+// });
+
+// store.dispatch({
+//   type: ActionTypes.UPDATE_CELL,
+//   payload: {
+//     id: order_id,
+//     content: 'updated',
+//   },
+// });
+
+// store.dispatch({
+//   type: ActionTypes.DELETE_CELL,
+//   payload: {
+//     id: order_id,
+//   },
+// });
+
+// console.log(store.getState());
