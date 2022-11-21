f3e942b822b8583c4f32e6bf93523b08a39c7baf
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-21 17:05:43 +0900

    create action bar

---
+++ src/components/action-bar.tsx
@@ -0,0 +1,11 @@
+const ActionBar: React.FC = () => {
+  return (
+    <div>
+      <button>Up</button>
+      <button>Down</button>
+      <button>Delete</button>
+    </div>
+  );
+};
+
+export default ActionBar;
