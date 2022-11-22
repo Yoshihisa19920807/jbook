04576a14481906f0540d537b1dfe9479dd38da74
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-22 17:15:17 +0900

    update bundle reducer

--- src/state/reducers/bundleReducer.ts
+++ src/state/reducers/bundleReducer.ts
@@ -1,1 +1,29 @@
-export {};
+import produce from 'immer';
+import { ActionTypes } from '../action-types';
+import { Action } from '../actions';
+
+interface BundlesState {
+  [key: string]: {
+    loading: boolean;
+    code: string;
+    err: string;
+  };
+}
+
+const initialState: BundlesState = {};
+
+const reducer = () =>
+  produce(
+    (state: BundlesState = initialState, action: Action): BundlesState => {
+      switch (action.type) {
+        case ActionTypes.BUNDLE_START:
+          return state;
+        case ActionTypes.BUNDLE_COMPLETE:
+          return state;
+        default:
+          return state;
+      }
+    }
+  );
+
+export default reducer;
