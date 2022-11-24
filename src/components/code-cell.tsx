08db6e1977258a57d0d2d6d961d78f2b34651ab7
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-24 14:40:43 +0900

    update bundle reducer

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -2,32 +2,30 @@ import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
-import bundle from '../bundler';
+import { BundlesState } from '../state/reducers/bundleReducer';
 import Resizable from './resizable';
 import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
+import { useTypedSelector } from '../hooks/use-typed-selector';
 
 interface CodeCellProps {
   cell: Cell;
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-  const [code, setCode] = useState('');
-  const [err, setErr] = useState('');
-
-  const { updateCell } = useActions();
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector((state) => state.bundles![cell.id]);
+  console.log(bundle);
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundle(cell.content);
-      setCode(output.code);
-      setErr(output.err);
+      createBundle(cell.id, cell.content);
     }, 1000);
     // Executed at the cleanup phase
     return () => {
       clearTimeout(timer);
     };
-  }, [cell.content]);
+  }, [cell.content, cell.id]);
 
   // const onClick = async () => {
   //   const output = await bundle(input);
@@ -51,10 +49,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             }}
           />
         </Resizable>
-        {/* <div>
-          <button onClick={onClick}>Submit</button>
-        </div> */}
-        <Preview code={code} err={err} />
+        {bundle && <Preview code={bundle.code} err={bundle.err} />}
       </div>
     </Resizable>
   );
--- src/state/reducers/bundleReducer.ts
+++ src/state/reducers/bundleReducer.ts
@@ -2,39 +2,40 @@ import produce from 'immer';
 import { ActionTypes } from '../action-types';
 import { Action } from '../actions';
 
-interface BundlesState {
-  [key: string]: {
-    loading: boolean;
-    code: string;
-    err: string;
-  };
+export interface BundlesState {
+  [key: string]:
+    | {
+        loading: boolean;
+        code: string;
+        err: string;
+      }
+    | undefined;
 }
 
 const initialState: BundlesState = {};
 
-const reducer = () =>
-  produce(
-    (state: BundlesState = initialState, action: Action): BundlesState => {
-      switch (action.type) {
-        case ActionTypes.BUNDLE_START:
-          state[action.payload.cellId] = {
-            loading: true,
-            code: '',
-            err: '',
-          };
-          return state;
-        case ActionTypes.BUNDLE_COMPLETE:
-          state[action.payload.cellId] = {
-            loading: false,
-            code: action.payload.bundle.code,
-            err: action.payload.bundle.err,
-          };
-          return state;
-        default:
-          return state;
-      }
-    },
-    initialState
-  );
+const reducer = produce(
+  (state: BundlesState = initialState, action: Action): BundlesState => {
+    switch (action.type) {
+      case ActionTypes.BUNDLE_START:
+        state[action.payload.cellId] = {
+          loading: true,
+          code: '',
+          err: '',
+        };
+        return state;
+      case ActionTypes.BUNDLE_COMPLETE:
+        state[action.payload.cellId] = {
+          loading: false,
+          code: action.payload.bundle.code,
+          err: action.payload.bundle.err,
+        };
+        return state;
+      default:
+        return state;
+    }
+  },
+  initialState
+);
 
 export default reducer;
