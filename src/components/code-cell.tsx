4a68ab6e0e206f108d51c7ffb72641d7f5d1784c
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-25 09:10:13 +0900

    - bugfix   - blink of the preview backgroud

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -49,10 +49,12 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
           />
         </Resizable>
         {!bundle || bundle.loading ? (
-          <div className="progress-cover">
-            <progress className="progress is-small is-primary" max="100">
-              Loading
-            </progress>
+          <div className="progress-wrapper">
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
           </div>
         ) : (
           <Preview code={bundle.code} err={bundle.err} />
