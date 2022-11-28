52d9e1a1566d956444f4fc18446a19a11ff4e37c
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-11-28 12:20:27 +0900

    modularize cumulative code

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -6,6 +6,7 @@ import Resizable from './resizable';
 import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import { useCumulativeCode } from '../hooks/use-cumulative-code';
 
 interface CodeCellProps {
   cell: Cell;
@@ -14,43 +15,7 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
-  const cumulativeCode = useTypedSelector((state) => {
-    const { data, order } = state.cells;
-    const orderedCells = order.map((id) => data[id]);
-    const showFunc = `
-      import _React from 'react'
-      import _ReactDOM from 'react-dom'
-      var show = (value) => {
-        if (typeof value === 'object') {
-          if (value.$$typeof && value.props) {
-            _ReactDOM.render(value, document.querySelector('#root'))
-          } else {
-            document.querySelector('#root').innerHTML = JSON.stringify(value);
-          }
-        } else{
-          document.querySelector('#root').innerHTML = value;
-        }
-      };
-    `;
-    const showFuncNoop = `var show = () => {}`;
-    const codeArray = [];
-    for (let c of orderedCells) {
-      if (c.type === 'code') {
-        if (c.id === cell.id) {
-          codeArray.push(showFunc);
-        } else {
-          codeArray.push(showFuncNoop);
-        }
-        codeArray.push(c.content);
-      }
-      if (c.id === cell.id) {
-        break;
-      }
-    }
-    return codeArray;
-  });
-
-  console.log(cumulativeCode);
+  const cumulativeCode = useCumulativeCode(cell.id);
 
   useEffect(() => {
     if (!bundle) {
